d3f1845837502576f4ec3088e6b07cf68e449bb9
FouadDevOps <algahmif@aetna.com>
2024-05-02 09:36:33 -0400

    add vaes

--- variable.tf
+++ variable.tf
@@ -1,5 +1,6 @@
 variable "client_id" {
   type = string
+  default = "276850cb-96f2-493a-898e-6e4f475fda04"
 }
 
 variable "client_secret" {
@@ -8,8 +9,10 @@ variable "client_secret" {
 
 variable "tenant_id" {
   type = string
+  default = "744200ba-c436-41f9-bed1-1f0ca4be3d41"
 }
 
 variable "subscription_id" {
   type = string
+  default = "71f5049e-863e-4095-812e-9920d55fbdae"
 }
