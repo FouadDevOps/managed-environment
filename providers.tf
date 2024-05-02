fd49f2cb49690b2bda8aa28f35d77c989e04a536
FouadDevOps <algahmif@aetna.com>
2024-05-02 11:58:12 -0400

    add vars

--- providers.tf
+++ providers.tf
@@ -12,10 +12,4 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  
-  client_secret = var.client_secret
-  client_id     = "e497a966-9f35-4af4-84f9-a543cf919483"
-  tenant_id        = "744200ba-c436-41f9-bed1-1f0ca4be3d41"
-  subscription_id  = "71f5049e-863e-4095-812e-9920d55fbdae"
-
 }
