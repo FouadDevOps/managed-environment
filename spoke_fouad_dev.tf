97e5e5e43368b9b628046c1c4f7bb52fbc6f379f
FouadDevOps <algahmif@aetna.com>
2024-05-02 09:34:08 -0400

    add vars

--- spoke_fouad_dev.tf
+++ spoke_fouad_dev.tf
@@ -8,9 +8,9 @@ aks_cluster = {
 
 }
 
-client_secret = var.client_secret
-client_id     = var.client_id
-tenant_id        = var.tenant_id
-subscription_id  = var.subscription_id
+  client_secret = var.client_secret
+  client_id     = "276850cb-96f2-493a-898e-6e4f475fda04"
+  tenant_id        = "744200ba-c436-41f9-bed1-1f0ca4be3d41"
+  subscription_id  = "71f5049e-863e-4095-812e-9920d55fbdae"
 
 }
