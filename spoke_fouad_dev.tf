3dd31ae2c0e67c5c88b0aa5bd7fc487681750104
FouadDevOps <algahmif@aetna.com>
2024-05-02 22:43:32 -0400

    fix formatting

--- spoke_fouad_dev.tf
+++ spoke_fouad_dev.tf
@@ -1,16 +1,16 @@
 module "spoke_fouad_dev" {
   source = "github.com/FouadDevOps/fouad_spoke_module?ref=main"
-    
 
 
-aks_cluster = {
+
+  aks_cluster = {
     name = "myaks_cluster"
 
-}
+  }
 
-  client_secret = var.client_secret
-  client_id     = "e497a966-9f35-4af4-84f9-a543cf919483"
-  tenant_id        = "744200ba-c436-41f9-bed1-1f0ca4be3d41"
-  subscription_id  = "71f5049e-863e-4095-812e-9920d55fbdae"
+  client_secret   = var.client_secret
+  client_id       = "e497a966-9f35-4af4-84f9-a543cf919483"
+  tenant_id       = "744200ba-c436-41f9-bed1-1f0ca4be3d41"
+  subscription_id = "71f5049e-863e-4095-812e-9920d55fbdae"
 
 }
--- variable.tf
+++ variable.tf
@@ -1,18 +1,18 @@
 variable "client_id" {
-  type = string
+  type    = string
   default = "e497a966-9f35-4af4-84f9-a543cf919483"
 }
 
 variable "client_secret" {
-    type = string
+  type = string
 }
 
 variable "tenant_id" {
-  type = string
+  type    = string
   default = "744200ba-c436-41f9-bed1-1f0ca4be3d41"
 }
 
 variable "subscription_id" {
-  type = string
+  type    = string
   default = "71f5049e-863e-4095-812e-9920d55fbdae"
 }
