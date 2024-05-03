3abdebc7ac5fbf31ab10e4f602992c451e226f83
FouadDevOps <algahmif@aetna.com>
2024-05-03 10:00:28 -0400

    add

--- fouad/backend.tf
+++ fouad/backend.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "example" {
   name                     = "fouad-azure-storage"
-  resource_group_name      = azurerm_resource_group.aks_resource_group.aks_rg.name
-  location                 = azurerm_resource_group.aks_resource_group.aks_rg.location
+  resource_group_name      = azurerm_resource_group.aks_rg.name
+  location                 = azurerm_resource_group.aks_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
