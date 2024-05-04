a1f88f3edfd219c20634fe19a9583a7d4b6c6f17
FouadDevOps <algahmif@aetna.com>
2024-05-03 21:52:42 -0400

    remove backend

--- fouad/backend.tf
+++ fouad/backend.tf
@@ -1,29 +1,29 @@
 
-resource "azurerm_storage_account" "sa" {
-  name                     = "fouadazurestorage"
-  resource_group_name      = "aks_resource_group"
-  location                 = "East US"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# resource "azurerm_storage_account" "sa" {
+#   name                     = "fouadazurestorage"
+#   resource_group_name      = "aks_resource_group"
+#   location                 = "East US"
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
 
-  tags = {
-    environment = "staging"
-  }
-}
+#   tags = {
+#     environment = "staging"
+#   }
+# }
 
-resource "azurerm_storage_container" "sc" {
-  name                  = "tfstate-container"
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
-}
+# resource "azurerm_storage_container" "sc" {
+#   name                  = "tfstate-container"
+#   storage_account_name  = azurerm_storage_account.sa.name
+#   container_access_type = "private"
+# }
 
 
-terraform {
-  backend "azurerm" {
-    resource_group_name = "aks_resource_group"
-    storage_account_name = "fouadazurestorage"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name = "aks_resource_group"
+#     storage_account_name = "fouadazurestorage"
+#     container_name = "tfstate"
+#     key = "terraform.tfstate"
+#   }
+# }
 
