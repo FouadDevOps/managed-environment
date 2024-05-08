9bc145297cb01a16e292a8ad7d013e73f5868f05
FouadDevOps <algahmif@aetna.com>
2024-05-08 13:27:54 -0400

    fetch a loadBalancerip

--- fouad/spoke_fouad_dev.tf
+++ fouad/spoke_fouad_dev.tf
@@ -4,7 +4,8 @@ module "spoke_fouad_dev" {
 
   aks_cluster = {
     name = "myaks_cluster"
-    loadBalancerIp = "1.2.3.4"
+    # loadBalancerIp = "1.2.3.4"
+    auto_loadBalancerIp = true
     service_mesh = "istio"
   }
 
