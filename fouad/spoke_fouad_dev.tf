59977ed5d5c107544b6127482ddf66eca37ac29c
FouadDevOps <algahmif@aetna.com>
2024-05-08 20:57:21 -0400

    provide ip

--- fouad/spoke_fouad_dev.tf
+++ fouad/spoke_fouad_dev.tf
@@ -4,7 +4,7 @@ module "spoke_fouad_dev" {
 
   aks_cluster = {
     name = "myaks_cluster"
-    auto_loadBalancerIp = true
+    auto_loadBalancerIp = "1.2.3.4"
     }
 
   client_secret = var.client_secret
