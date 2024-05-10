ffe9acba1f47c3ecd192f508867d7fa00a6e9cde
FouadAlgahmi <fouadalgahmi7@gmail.com>
2024-05-10 16:19:57 -0400

    fetch an ip

--- fouad/spoke_fouad_dev.tf
+++ fouad/spoke_fouad_dev.tf
@@ -4,7 +4,7 @@ module "spoke_fouad_dev" {
 
   aks_cluster = {
     name = "myaks_cluster"
-    auto_loadBalancerIp = false
+    auto_loadBalancerIp = true
     }
 
   client_secret = var.client_secret
