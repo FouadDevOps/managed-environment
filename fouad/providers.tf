bbf657a489619adf992973f00b743f9f1cf331b5
FouadDevOps <algahmif@aetna.com>
2024-05-22 09:52:34 -0400

    update creds

--- fouad/providers.tf
+++ fouad/providers.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
     features {}
 
-  client_id       = "4b5a1407-aec8-4430-a3ff-bd6bf21d58e0"
+  client_id       = "9b47d401-29f3-443d-9db1-1c0aff07dedd"
   client_secret   = var.client_secret
   tenant_id       = "744200ba-c436-41f9-bed1-1f0ca4be3d41"
   subscription_id = "6d8f5795-12dd-4f05-82f3-47e1830275c7"
